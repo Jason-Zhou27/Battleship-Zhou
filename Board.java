153a986f2a0ee2142a3e97cf4ecaffc26534a2ab
Jason-Zhou27 <zhoujas27@7hills.org>
2025-10-16 18:38:55 -0400

    Board.java edit

--- Board.java
+++ Board.java
@@ -22,6 +22,17 @@ public class Board {
 	}
 	//methods
 	
+	//return representation of the board
+	public String toString(){
+		//use a nested for loop to print out row by row
+		for (r=0;r<10;r++){
+			for (c=0;c<10;c++){
+				System.out.print(squares[r][c]);
+				System.out.print(" ");
+			}
+			System.out.print("\n"); //after a row of dashes is completed, create a new line
+		}
+	}
 	
 
 
