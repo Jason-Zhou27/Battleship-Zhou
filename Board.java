e814a4b8c67bcc60491440f7f0e6dd351aa98382
Jason-Zhou27 <zhoujas27@7hills.org>
2025-10-16 19:09:22 -0400

    added comments for additional clarification; fixed the issue with null showing up before by initializing the concactenation strings with

--- Board.java
+++ Board.java
@@ -9,7 +9,7 @@ public class Board {
 	//constructor(s)
 	public Board(){
 		//initialize the values for a 10x10 array from Runestone 4.11.3
-		//the size of the array (10x10) is determined from the values given
+		//the size of the array (10x10) is determined from the values given (Runestone)
 		squares1 = new String[][] { 
 			{"-","-","-","-","-","-","-","-","-","-",},
 			{"-","-","-","-","-","-","-","-","-","-",},
@@ -42,26 +42,21 @@ public class Board {
 	public String toString(){
 		//use a nested for loop to concactenate row by row of square1
 		for (int r=0;r<10;r++){
+			//within a row, print out the values by shifting column in the 2d array 
 			for (int c=0;c<10;c++){
-				strSquares1 = strSquares1 + (squares1[r][c]) + " ";
+				strSquares1 = strSquares1 + (squares1[r][c]) + " "; //use the format: existing concactenation + character from particular position + space
 			}
 			strSquares1 = strSquares1 + ("\n"); //after a row of dashes is completed, create a new line
 		}
 		
-		//use a nested for loop to print out row by row of square2
+		//use a nested for loop to concactenate row by row of square2
 		for (int r=0;r<10;r++){
+			//within a row, print out the values by shifting column in the 2d array
 			for (int c=0;c<10;c++){
-				strSquares2 = strSquares2 + (squares2[r][c]) + " ";
+				strSquares2 = strSquares2 + (squares2[r][c]) + " "; //use the format: existing concactenation + character from particular position + space
 			}
-			strSquares1 = strSquares1 + ("\n"); //after a row of dashes is completed, create a new line
+			strSquares2 = strSquares2 + ("\n"); //after a row of dashes is completed, create a new line
 		}
-		return (strSquares1 + "\n\n" + strSquares2);
+		return (strSquares1 + "\n\n" + strSquares2); //print the two squares out, separating the two by two line breaks
 	}
-	
-
-
-
-
-
-
 }
