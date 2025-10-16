9144f668affab07f69534887e5332e18e58bea8f
Jason-Zhou27 <zhoujas27@7hills.org>
2025-10-16 19:02:17 -0400

    changed print to return; created tester class to see functionality and fix syntax errors; developed concactenation within toString method b/c no longer print but return; fixed for loop syntax errors specifically

--- Board.java
+++ Board.java
@@ -1,13 +1,16 @@
 public class Board {
 
 	//variables
-	private int String[][] squares; //declare 2D array of Strings called squares
+	private String[][] squares1; //declare 2D array of Strings called squares1
+	private String[][] squares2; //declare 2D array of Strings called squares2
+	private String strSquares1 = "";
+	private String strSquares2 = "";
 	
 	//constructor(s)
 	public Board(){
 		//initialize the values for a 10x10 array from Runestone 4.11.3
 		//the size of the array (10x10) is determined from the values given
-		squares = { 
+		squares1 = new String[][] { 
 			{"-","-","-","-","-","-","-","-","-","-",},
 			{"-","-","-","-","-","-","-","-","-","-",},
 			{"-","-","-","-","-","-","-","-","-","-",},
@@ -18,20 +21,41 @@ public class Board {
 			{"-","-","-","-","-","-","-","-","-","-",},
 			{"-","-","-","-","-","-","-","-","-","-",},
 			{"-","-","-","-","-","-","-","-","-","-",}
-		}
+		};
+		
+		squares2 = new String[][] { 
+			{"-","-","-","-","-","-","-","-","-","-",},
+			{"-","-","-","-","-","-","-","-","-","-",},
+			{"-","-","-","-","-","-","-","-","-","-",},
+			{"-","-","-","-","-","-","-","-","-","-",},
+			{"-","-","-","-","-","-","-","-","-","-",},
+			{"-","-","-","-","-","-","-","-","-","-",},
+			{"-","-","-","-","-","-","-","-","-","-",},
+			{"-","-","-","-","-","-","-","-","-","-",},
+			{"-","-","-","-","-","-","-","-","-","-",},
+			{"-","-","-","-","-","-","-","-","-","-",}
+		};
 	}
 	//methods
 	
 	//return representation of the board
 	public String toString(){
-		//use a nested for loop to print out row by row
-		for (r=0;r<10;r++){
-			for (c=0;c<10;c++){
-				System.out.print(squares[r][c]);
-				System.out.print(" ");
+		//use a nested for loop to concactenate row by row of square1
+		for (int r=0;r<10;r++){
+			for (int c=0;c<10;c++){
+				strSquares1 = strSquares1 + (squares1[r][c]) + " ";
 			}
-			System.out.print("\n"); //after a row of dashes is completed, create a new line
+			strSquares1 = strSquares1 + ("\n"); //after a row of dashes is completed, create a new line
 		}
+		
+		//use a nested for loop to print out row by row of square2
+		for (int r=0;r<10;r++){
+			for (int c=0;c<10;c++){
+				strSquares2 = strSquares2 + (squares2[r][c]) + " ";
+			}
+			strSquares1 = strSquares1 + ("\n"); //after a row of dashes is completed, create a new line
+		}
+		return (strSquares1 + "\n\n" + strSquares2);
 	}
 	
 
---
+++ Tester.java
@@ -0,0 +1,9 @@
+public class Tester{
+
+	public static void main(String[] args){
+		Board g1 = new Board();
+		System.out.println(g1.toString());
+	}
+
+
+}
